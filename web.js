21a458a1bc150e7b15f67bd65b9a04b573711059
YIP Chun-Ho <jy200x@gmail.com>
2013-07-08 16:40:14 +0800

    reconstruct repo

---
+++ web.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var fs = require('fs');
+
+var app = express.createServer(express.logger());
+
+var buf = new Buffer(fs.readFileSync('index.html'), 'utf-e');
+buf = buf.toString();
+
+app.get('/', function(request, response) {
+  response.send(buf);
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
